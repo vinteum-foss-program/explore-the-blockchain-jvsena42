c2a17f558479c881e7a8ed340ced2a15f0eb0523
João Victor Sena <jvsena16@gmail.com>
2024-11-24 16:07:26 -0300

    Update 004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -1,6 +1,12 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
 
-DESCRIPTOR_WITH_CHECKSUM=$(bitcoin-cli getdescriptorinfo "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/0/100)" | jq -r '.descriptor')
-DERIVED_ADDRESSES = $(bitcoin-cli deriveaddresses $DESCRIPTOR_WITH_CHECKSUM)
+XPUB="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
+INDEX="100"
+BASE_DESCRIPTOR="tr($XPUB/0/$INDEX)"
+
+DESCRIPTOR_WITH_CHECKSUM=$(bitcoin-cli getdescriptorinfo "$BASE_DESCRIPTOR" | jq -r '.descriptor')
+
+DERIVED_ADDRESSES=$(bitcoin-cli deriveaddresses $DESCRIPTOR_WITH_CHECKSUM)
+
 echo $DERIVED_ADDRESSES
