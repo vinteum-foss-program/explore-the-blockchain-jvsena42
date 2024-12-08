8a8cee39f879b34326048cc4f9edcf9a6e5da718
João Victor Sena <jvsena16@gmail.com>
2024-12-08 17:01:30 -0300

    Update 004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -3,7 +3,7 @@
 
 XPUB="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
 INDEX="100"
-BASE_DESCRIPTOR="tr($XPUB/0/$INDEX)"
+BASE_DESCRIPTOR="tr([$XPUB/0/$INDEX])"
 DESCRIPTOR_WITH_CHECKSUM=$(bitcoin-cli getdescriptorinfo "$BASE_DESCRIPTOR" | jq -r '.descriptor')
 DERIVED_ADDRESS=$(bitcoin-cli deriveaddresses "$DESCRIPTOR_WITH_CHECKSUM" | jq -r '.[0]')
 echo $DERIVED_ADDRESS
